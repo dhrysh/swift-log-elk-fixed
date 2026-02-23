99a921083c31bdd995a3911503f96e4c38c09f88
Dmytro <68398868+dhrysh@users.noreply.github.com>
2026-02-23 01:40:29 -0800

    Change writeData to writeBytes for log data

--- Sources/LoggingELK/LogstashLogHandler.swift
+++ Sources/LoggingELK/LogstashLogHandler.swift
@@ -263,7 +263,7 @@ public struct LogstashLogHandler: LogHandler {
         // Write size of the log data
         Self.byteBuffer?.writeInteger(logData.count)
         // Write actual log data to log store
-        Self.byteBuffer?.writeData(logData)
+        Self.byteBuffer?.writeBytes(logData)
 
         Self.byteBufferLock.unlock()
     }
