cd1ff883b1510f5328ce074c42ba4ef1364750d4
Dmytro <68398868+dhrysh@users.noreply.github.com>
2026-02-23 00:32:14 -0800

    Update async-http-client dependency version

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.0"),
-        .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMinor(from: "1.5.0"))
+        .package(url: "https://github.com/swift-server/async-http-client.git", .upToNextMinor(from: "1.25.0"))
     ],
     targets: [
         .target(
